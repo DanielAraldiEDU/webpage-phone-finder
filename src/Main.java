fb3bfa4cdff8914c94c733f299e9084aec420e74
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-15 18:15:02 -0300

    feat: add `CapturaRecursosWeb` service

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package buscapadraoweb;
-
-import buscaweb.CapturaRecursosWeb;
+import api.CapturaRecursosWeb;
 import java.util.ArrayList;
 
 /**
@@ -15,9 +9,9 @@ import java.util.ArrayList;
 public class Main {
 
     // busca char em vetor e retorna indice
-    public static int get_char_ref (char[] vet, char ref ) {
-        for (int i=0; i<vet.length; i++ ) {
-            if (vet[i] == ref){
+    public static int get_char_ref(char[] vet, char ref) {
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] == ref) {
                 return i;
             }
         }
@@ -25,8 +19,8 @@ public class Main {
     }
 
     // busca string em vetor e retorna indice
-    public static int get_string_ref (String[] vet, String ref ) {
-        for (int i=0; i<vet.length; i++ ) {
+    public static int get_string_ref(String[] vet, String ref) {
+        for (int i = 0; i < vet.length; i++) {
             if (vet[i].equals(ref)) {
                 return i;
             }
@@ -34,10 +28,8 @@ public class Main {
         return -1;
     }
 
-    
-
-    //retorna o próximo estado, dado o estado atual e o símbolo lido
-    public static int proximo_estado(char[] alfabeto, int[][] matriz,int estado_atual,char simbolo) {
+    // retorna o próximo estado, dado o estado atual e o símbolo lido
+    public static int proximo_estado(char[] alfabeto, int[][] matriz, int estado_atual, char simbolo) {
         int simbol_indice = get_char_ref(alfabeto, simbolo);
         if (simbol_indice != -1) {
             return matriz[estado_atual][simbol_indice];
@@ -50,14 +42,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //instancia e usa objeto que captura código-fonte de páginas Web
+        // instancia e usa objeto que captura código-fonte de páginas Web
         CapturaRecursosWeb crw = new CapturaRecursosWeb();
         crw.getListaRecursos().add("https://www.univali.br/");
         ArrayList<String> listaCodigos = crw.carregarRecursos();
 
         String codigoHTML = listaCodigos.get(0);
 
-        //mapa do alfabeto
+        // mapa do alfabeto
         char[] alfabeto = new char[10];
         alfabeto[0] = '0';
         alfabeto[1] = '1';
@@ -70,7 +62,7 @@ public class Main {
         alfabeto[8] = '8';
         alfabeto[9] = '9';
 
-        //mapa de estados
+        // mapa de estados
         String[] estados = new String[3];
         estados[0] = "q0";
         estados[1] = "q1";
@@ -78,13 +70,13 @@ public class Main {
 
         String estado_inicial = "q0";
 
-        //estados finais
+        // estados finais
         String[] estados_finais = new String[1];
         estados_finais[0] = "q2";
 
-        //tabela de transição de AFD para reconhecimento números de dois dígitos
+        // tabela de transição de AFD para reconhecimento números de dois dígitos
         int[][] matriz = new int[3][10];
-        //transições de q0
+        // transições de q0
         matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, '0')] = get_string_ref(estados, "q1");
         matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, '1')] = get_string_ref(estados, "q1");
         matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, '2')] = get_string_ref(estados, "q1");
@@ -95,7 +87,7 @@ public class Main {
         matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, '7')] = get_string_ref(estados, "q1");
         matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, '8')] = get_string_ref(estados, "q1");
         matriz[get_string_ref(estados, "q0")][get_char_ref(alfabeto, '9')] = get_string_ref(estados, "q1");
-        //transições de q1
+        // transições de q1
         matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, '0')] = get_string_ref(estados, "q2");
         matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, '1')] = get_string_ref(estados, "q2");
         matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, '2')] = get_string_ref(estados, "q2");
@@ -106,7 +98,7 @@ public class Main {
         matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, '7')] = get_string_ref(estados, "q2");
         matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, '8')] = get_string_ref(estados, "q2");
         matriz[get_string_ref(estados, "q1")][get_char_ref(alfabeto, '9')] = get_string_ref(estados, "q2");
-        //transições de q2
+        // transições de q2
         matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, '0')] = -1;
         matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, '1')] = -1;
         matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, '2')] = -1;
@@ -118,41 +110,41 @@ public class Main {
         matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, '8')] = -1;
         matriz[get_string_ref(estados, "q2")][get_char_ref(alfabeto, '9')] = -1;
 
-        int estado = get_string_ref (estados, estado_inicial);
+        int estado = get_string_ref(estados, estado_inicial);
         int estado_anterior = -1;
         ArrayList<String> palavras_reconhecidas = new ArrayList();
 
         String palavra = "";
 
-        //varre o código-fonte de um código
-        for (int i=0; i<codigoHTML.length(); i++) {
+        // varre o código-fonte de um código
+        for (int i = 0; i < codigoHTML.length(); i++) {
             estado_anterior = estado;
             estado = proximo_estado(alfabeto, matriz, estado, codigoHTML.charAt(i));
-            //se o não há transição
+            // se o não há transição
             if (estado == -1) {
-                //pega estado inicial
+                // pega estado inicial
                 estado = get_string_ref(estados, estado_inicial);
                 // se o estado anterior foi um estado final
                 if (get_string_ref(estados_finais, estados[estado_anterior]) != -1) {
-                    //se a palavra não é vazia adiciona palavra reconhecida
-                    if ( ! palavra.equals("")){
+                    // se a palavra não é vazia adiciona palavra reconhecida
+                    if (!palavra.equals("")) {
                         palavras_reconhecidas.add(palavra);
                     }
                     // se ao analisar este caracter não houve transição
                     // teste-o novamente, considerando que o estado seja inicial
                     i--;
                 }
-                //zera palavra
+                // zera palavra
                 palavra = "";
             } else {
-                //se houver transição válida, adiciona caracter a palavra
+                // se houver transição válida, adiciona caracter a palavra
                 palavra += codigoHTML.charAt(i);
             }
         }
 
-        //foreach no Java para exibir todas as palavras reconhecidas
-        for (String p: palavras_reconhecidas){
-            System.out.println (p);
+        // foreach no Java para exibir todas as palavras reconhecidas
+        for (String p : palavras_reconhecidas) {
+            System.out.println(p);
         }
     }
 }
